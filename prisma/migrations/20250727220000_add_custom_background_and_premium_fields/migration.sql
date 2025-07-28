3f9ba277a0e56cec8869dda20dfb6f58dd8bb98d
BolsaDigital <bolsadigitalvenezuela@gmail.com>
2025-07-27 21:37:54 -0400

    feat: Manually created migration for isCustomBackground and isPremium fields

---
+++ prisma/migrations/20250727220000_add_custom_background_and_premium_fields/migration.sql
@@ -0,0 +1,5 @@
+-- AlterTable
+ALTER TABLE "media" ADD COLUMN "isCustomBackground" BOOLEAN NOT NULL DEFAULT false;
+
+-- AlterTable
+ALTER TABLE "media" ADD COLUMN "isPremium" BOOLEAN NOT NULL DEFAULT false;
